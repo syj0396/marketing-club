3a02518b39e96e3d2843c2c628cbc2099781bbf7
syj0396 <syj0396@naver.com>
2022-07-28 11:10:55 +0900

    navbar 상단고정

--- src/components/Navbar/Navbars.tsx
+++ src/components/Navbar/Navbars.tsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom'
+import './style.css'
 export function Navbars() {
     return(
-        <div className="nav">
-            <h1>Marketing Club</h1>
+        <div className="nav shadow-sm">
+            <Link to="/"><li><h1 className="nav-title">Marketing Club</h1></li></Link>
+            <ul className="nav-links">
+                <Link to = "/class"><li>CLASS</li></Link>
+                <Link to = "/community"><li>COMMUNITY</li></Link>
+                <Link to = "/myPage"><li>MY</li></Link>
+            </ul>
         </div>
     )
 }
