3c6e6dba026cd689cac3bdc98fed65989cc61638
syj0396 <syj0396@naver.com>
2022-07-28 18:28:20 +0900

    navbar css 수정

--- src/components/Navbar/Navbars.tsx
+++ src/components/Navbar/Navbars.tsx
@@ -3,11 +3,11 @@ import './style.css'
 export function Navbars() {
     return(
         <div className="nav shadow-sm">
-            <Link to="/"><li><h1 className="nav-title">Marketing Club</h1></li></Link>
+            <Link className="nav-title" to="/"><li><h1>Marketing Club</h1></li></Link>
             <ul className="nav-links">
-                <Link to = "/class"><li>CLASS</li></Link>
-                <Link to = "/community"><li>COMMUNITY</li></Link>
-                <Link to = "/myPage"><li>MY</li></Link>
+                <Link className="nav-list" to = "/class"><li>CLASS</li></Link>
+                <Link className="nav-list" to = "/community"><li>COMMUNITY</li></Link>
+                <Link className="nav-list" to = "/myPage"><li>MY</li></Link>
             </ul>
         </div>
     )
